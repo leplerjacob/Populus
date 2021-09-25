b6b8dc0e089b6c03f8a481f84b7c6fbc18be6a31
Jacob Lepler <lepler.jacob@gmail.com>
2021-09-25 14:36:17 -0400

    add create user request

--- populus-backend/app/controllers/users_controller.rb
+++ populus-backend/app/controllers/users_controller.rb
@@ -1,6 +1,12 @@
 class UsersController < ApplicationController
     def create
-        print(user_params)
+        if user_params
+            new_user = User.new(user_params)
+            if new_user.save()
+                render json: {status: 200, username: new_user.username}
+            end
+        end
+        render json: {status: 401}
     end
 
     def user_params
