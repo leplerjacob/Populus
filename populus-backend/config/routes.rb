47f12f16e5376f8bd4baeabb059a9966374924aa
Jacob Lepler <lepler.jacob@gmail.com>
2021-09-26 23:31:43 -0400

    add photo-upload POST route

--- populus-backend/config/routes.rb
+++ populus-backend/config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resource :photos
+  
 
   post '/users', to: 'users#create'
   post '/login', to: 'users#login'
+  post '/photo-upload', to: 'photos#upload_photo'
 end
